470bf564a85787a58167583c892823d2ce8d03d0
Kanashin_Dmitry <dmitrii.a.kanashin@gmail.com>
2020-08-19 18:16:48 +0300

    Only Runge function testing init 101 data, picking knots from mesh of size 1001

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
 import numpy as np
-from scipy.interpolate import LSQUnivariateSpline, CubicSpline
+from scipy.interpolate import make_lsq_spline, CubicSpline
 from prettytable import PrettyTable
 import pandas as pd
 
 
 class VariationalApproach:
-    def __init__(self, x_samples, y_samples, knots, test_x, test_y, err):
+    def __init__(self, x_samples, y_samples, knots, test_x, test_y, shift):
         self.x = x_samples
         self.y = y_samples
         self.test_x = test_x
         self.test_y = test_y
         self.knots = knots
-        self.err_bound = err
-        self.best_knots, self.lsq_spline, self.tol = self.find_best_knots()
+        self.shift = shift
+        self.best_knots, self.lsq_spline, self.tol, self.cubic_spl_err = self.find_best_knots()
 
-    shift = 0.1
+    degree = 3
+    Zero = 1.0e-12
+    err_bound = 0
 
     # algorithm
     def find_best_knots(self):
         # step 1
         cubic_spl = CubicSpline(self.x, self.y)
-        # print(self.size_sample, len(self.knots), max(abs(cubic_spl(self.test_x) - self.test_y)))
+
+        err_on_cubic_spl = max(abs(cubic_spl(self.test_x) - self.test_y))
+
+        self.err_bound = 2 * err_on_cubic_spl
 
         ranks = self.rank_knots(cubic_spl, self.knots)
 
-        min_n = len(self.knots) // 10
+        min_n = len(self.knots) // 100
         knots, lsq_spline, err_spl = self.find_n_best_knots(cubic_spl, ranks, min_n)
 
-        return knots, lsq_spline, err_spl
+        return knots, lsq_spline, err_spl, err_on_cubic_spl
 
     # step 2
     def rank_knots(self, spline, knots):
-        ranks = np.zeros(len(self.knots))
+        ranks = np.zeros(len(knots))
         jumps = 2 * abs(spline(knots[1:-1] + self.shift, 3) - spline(knots[1:-1] - self.shift, 3))
 
         for index in range(1, len(knots) - 1):
@@ -56,14 +61,21 @@ class VariationalApproach:
             indices = np.sort(indices_max[-num:][::-1])
 
             new_knots = [knots[i] for i in indices if i != last_knot and i != 0]
+            # new_knots = [knots[i] for i in indices
+            #             if i != last_knot and i != 0 and abs(init_spline(knots[i], 1)) >= self.Zero]
             new_knots = np.array(new_knots)
 
-            lsq_spline = LSQUnivariateSpline(self.x, self.y, new_knots)
+            new_knots = np.r_[(self.x[0],) * (self.degree + 1)
+                              , new_knots
+                              , (self.x[-1],) * (self.degree + 1)]
+
+            lsq_spline = make_lsq_spline(self.x, self.y, new_knots)
+
             err_spl = max(abs(lsq_spline(test_x) - init_spline(test_x)))
             if err_spl < self.err_bound:
-                return new_knots, lsq_spline, err_spl
+                return new_knots[self.degree + 1: -(self.degree + 1)], lsq_spline, err_spl
 
-        return new_knots, lsq_spline, err_spl
+        return new_knots[self.degree + 1: -self.degree], lsq_spline, err_spl
 
 
 def runge_function(x):
@@ -71,21 +83,25 @@ def runge_function(x):
 
 
 def sin_p_x_function(x):
-    return x + np.sin(3*x)
+    return x + np.sin(3 * x)
 
 
-def test_on_function(func, left_bound, right_bound, n_samples, init_n_knots, err_bound):
+def sin_p_x2_function(x):
+    return x ** 2 + np.sin(3 * x)
+
+
+def test_on_function(func, left_bound, right_bound, n_samples, init_n_knots, shift):
     x_samples = np.linspace(left_bound, right_bound, n_samples)
-    test_x = np.linspace(left_bound, right_bound, n_samples * 3)
+    test_x = np.linspace(left_bound, right_bound, n_samples * 10)
 
     knots = np.linspace(left_bound, right_bound, init_n_knots)
 
     y_samples = func(x_samples)
     test_y = func(test_x)
 
-    alg = VariationalApproach(x_samples, y_samples, knots, test_x, test_y, err_bound)
+    alg = VariationalApproach(x_samples, y_samples, knots, test_x, test_y, shift)
 
-    return len(alg.best_knots), alg.tol
+    return len(alg.best_knots), alg.tol, alg.cubic_spl_err
 
 
 def write_results_txt(name, heading, data):
@@ -99,33 +115,37 @@ def write_results_xls(name, data, heading):
     df.to_excel(excel_writer=name)
 
 
+def make_experience():
+    headers = ["Num samples", "Initial num of knots", "Best num interior knots"
+               , "Shift", "Cubic tolerance", "Tolerance"]
+    file_names = ['results_runge_100_1000.txt', 'results_sin_e-04.txt'
+                  , 'results_sin_p_x_e-04.txt', 'results_sin_p_x2_e-04.txt']
+    # paths = ["D:/variational_approach/" + file_names[:-4] + ".xlsx" for i in range(len(file_names))]
+    names = ["runge results\n", "sinus results\n"
+             , "sinus plus x results\n", "sinus plus square(x) results\n"]
+    intervals = [(-5, 5), (0, 2 * np.pi), (-2, 2), (-3, 3)]
+    functions = [runge_function, np.sin, sin_p_x_function, sin_p_x2_function]
+
+    table = PrettyTable(headers)
+    # table = []
+
+    for i in range(0, len(functions[:1])):  # testing only runge function
+        for num_samples in range(101, 102):
+            for init_num_knots in range(1001, 1002):
+                shift = (intervals[i][1] - intervals[i][0]) / (init_num_knots - 1) / 2
+                num_best_knots, tolerance, cubic_spl_err = test_on_function(functions[i], intervals[i][0]
+                                                                            , intervals[i][1], num_samples
+                                                                            , init_num_knots, shift)
+
+                table.add_row([num_samples, init_num_knots, num_best_knots, shift, cubic_spl_err, tolerance])
+                # table.append([num_samples, init_num_knots, num_best_knots, shift, cubic_spl_err, tolerance])
+
+        write_results_txt(file_names[i], names[i], table.get_string())
+        # write_results_xls(paths[i], table, headers)
+
+        table.clear_rows()
+        # table = []
+
+
 if __name__ == "__main__":
-    headers = ["Num samples", "Initial num of knots", "Best num interior knots", "Tolerance"]
-    table_runge = PrettyTable(headers)
-    table_sin = PrettyTable(headers)
-    table_sin_p_x = PrettyTable(headers)
-    # table_runge = []
-    # table_sin = []
-    # table_sin_p_x = []
-
-    for num_samples in range(20, 250, 10):
-        for init_num_knots in range(num_samples // 3, num_samples - 3):
-            best_knots, tolerance = test_on_function(runge_function, -5, 5, num_samples, init_num_knots, 1.0e-3)
-            table_runge.add_row([num_samples, init_num_knots, best_knots, tolerance])
-            # table_runge.append([num_samples, init_num_knots, best_knots, tolerance])
-
-            best_knots, tolerance = test_on_function(np.sin, 0, 2*np.pi, num_samples, init_num_knots, 1.0e-3)
-            table_sin.add_row([num_samples, init_num_knots, best_knots, tolerance])
-            # table_sin.append([num_samples, init_num_knots, best_knots, tolerance])
-
-            best_knots, tolerance = test_on_function(sin_p_x_function, -2, 2, num_samples, init_num_knots, 1.0e-3)
-            table_sin_p_x.add_row([num_samples, init_num_knots, best_knots, tolerance])
-            # table_sin_p_x.append([num_samples, init_num_knots, best_knots, tolerance])
-
-    # write_results_xls("D:/variational_approach/results_runge_e-03.xlsx", table_runge, headers)
-    # write_results_xls("D:/variational_approach/results_sin_e-03.xlsx", table_sin, headers)
-    # write_results_xls("D:/variational_approach/results_sinpx_e-03.xlsx", table_sin_p_x, headers)
-
-    write_results_txt('results_runge_e-03.txt', "runge results\n", table_runge.get_string())
-    write_results_txt('results_sin_e-03.txt', "sinus results\n", table_sin.get_string())
-    write_results_txt('results_sin_p_x_e-03.txt', "sinus plus x results\n", table_sin_p_x.get_string())
+    make_experience()
